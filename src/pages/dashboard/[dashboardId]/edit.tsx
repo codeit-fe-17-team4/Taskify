66e426b0f2b30621dafff4d0f96c81eccf63fa0d
yejinshim <simyejin9934@gmail.com>
2025-09-06 04:57:11 +0900

    design: 잠시 수정

--- src/pages/dashboard/[dashboardId]/edit.tsx
+++ src/pages/dashboard/[dashboardId]/edit.tsx
@@ -56,25 +56,26 @@ export default function MydashboardEdit(): ReactNode {
 
   return (
     <div className='min-h-screen bg-gray-50'>
-      {/* w-full 로 반응형 구현을 위한 mr-120 설정 */}
       <div className='mr-120 ml-5 min-w-2xs pt-5'>
-        <Link href='/dashboard/${dashboardId}'>
-          <div className='flex w-full max-w-4xl gap-4'>
+        <Link href={`/dashboard/${dashboardId}`}>
+          <div className='flex w-full cursor-pointer items-center gap-2'>
             <Image
               src='/icon/goback.svg'
               alt='go-back'
               width={10}
               height={10}
             />
-            <span className='cursor-pointer'>돌아가기</span>
-          </div>{' '}
+            <span>돌아가기</span>
+          </div>
         </Link>
+
+        {/* 대시보드 정보 수정 */}
         <div className='tablet:min-w-lg mobile:min-w-2xs tablet:w-full mobile:w-full mt-8 h-[340px] w-[620px] rounded-lg bg-white p-7'>
           <h2 className='text-xl font-bold'>{currentDashboard?.name}</h2>
           <form className='mt-4 space-y-4'>
             {/* 이름 수정 */}
             <div>
-              <label htmlFor='name' className='block text-base font-medium'>
+              <label htmlFor='name' className='font-sm block text-base'>
                 대시보드 이름
               </label>
               <input
@@ -136,7 +137,7 @@ export default function MydashboardEdit(): ReactNode {
                 <button className='flex h-7 w-7 cursor-pointer items-center justify-center rounded border border-gray-200 bg-white hover:bg-gray-100'>
                   <Image
                     src='/icon/prevPage.svg'
-                    alt='prev'
+                    alt='이전 페이지'
                     width={7}
                     height={7}
                   />
@@ -144,7 +145,7 @@ export default function MydashboardEdit(): ReactNode {
                 <button className='flex h-7 w-7 cursor-pointer items-center justify-center rounded border border-gray-200 bg-white hover:bg-gray-100'>
                   <Image
                     src='/icon/nextPage.svg'
-                    alt='prev'
+                    alt='다음 페이지'
                     width={7}
                     height={7}
                   />
@@ -171,7 +172,7 @@ export default function MydashboardEdit(): ReactNode {
                   >
                     <td className='py-3'>
                       <div className='flex items-center gap-2'>
-                        <div className='flex h-5 w-5 items-center justify-center rounded-full bg-sky-200 text-white'>
+                        <div className='flex h-5 w-5 items-center justify-center rounded-full bg-sky-200 text-sm text-white'>
                           {member.initial}
                         </div>
                         <span>{member.name}</span>
@@ -199,13 +200,13 @@ export default function MydashboardEdit(): ReactNode {
           <div className='flex items-center justify-between pr-8 pl-8'>
             <h2 className='text-xl font-bold'>초대 내역</h2>
             <div className='mobile:absolute mobile:right-5 mobile:top-5 flex items-center justify-end gap-3'>
-              <div className='flex items-center justify-end gap-3'>
+              <div className='flex items-center justify-end gap-2'>
                 <span className='text-xs'>1 페이지 중 1</span>
                 <div className='flex items-center justify-center'>
                   <button className='flex h-7 w-7 cursor-pointer items-center justify-center rounded border border-gray-200 bg-white hover:bg-gray-100'>
                     <Image
                       src='/icon/prevPage.svg'
-                      alt='prev'
+                      alt='이전 페이지'
                       width={7}
                       height={7}
                     />
@@ -213,27 +214,25 @@ export default function MydashboardEdit(): ReactNode {
                   <button className='flex h-7 w-7 cursor-pointer items-center justify-center rounded border border-gray-200 bg-white hover:bg-gray-100'>
                     <Image
                       src='/icon/nextPage.svg'
-                      alt='prev'
+                      alt='다음 페이지'
                       width={7}
                       height={7}
                     />
                   </button>
                 </div>
               </div>
-              <div>
-                <button
-                  className='mobile:absolute mobile:right-3 mobile:top-10 flex cursor-pointer gap-2 rounded-sm bg-violet-500 py-1 pr-2 pl-2 text-white hover:bg-violet-600'
-                  onClick={handleOpenModal}
-                >
-                  <Image
-                    src='/icon/addmember.svg'
-                    alt='add-member'
-                    width={20}
-                    height={20}
-                  />
-                  <span className='text-xs'>초대하기</span>
-                </button>
-              </div>
+              <button
+                className='mobile:absolute mobile:right-3 mobile:top-10 flex cursor-pointer items-center gap-2 rounded-sm bg-violet-500 py-1 pr-2 pl-2 text-white hover:bg-violet-600'
+                onClick={handleOpenModal}
+              >
+                <Image
+                  src='/icon/addmember.svg'
+                  alt='add-member'
+                  width={20}
+                  height={20}
+                />
+                <span className='text-xs'>초대하기</span>
+              </button>
             </div>
           </div>
           <div>
@@ -252,7 +251,9 @@ export default function MydashboardEdit(): ReactNode {
                   return (
                     <tr
                       key={member.id}
-                      className={`flex items-center justify-between pr-8 pl-8 ${!isLastItem ? 'border-b border-gray-200' : ''}`}
+                      className={`flex items-center justify-between pr-8 pl-8 ${
+                        !isLastItem ? 'border-b border-gray-200' : ''
+                      }`}
                     >
                       <td className='py-3'>
                         <div className='flex items-center gap-2'>
@@ -277,24 +278,24 @@ export default function MydashboardEdit(): ReactNode {
             </table>
           </div>
         </div>
+        {/* 대시보드 삭제 */}
+        <div>
+          <button
+            type='submit'
+            className='mobile:max-w-2xs my-6 h-12 w-xs cursor-pointer rounded-sm border border-gray-300 px-4 py-2 text-sm font-medium hover:bg-gray-100'
+            onClick={() => {
+              alert('대시보드를 삭제하시겠습니까?');
+            }}
+          >
+            대시보드 삭제하기
+          </button>
+        </div>
+        <InviteMemberModal
+          isOpen={isModalOpen}
+          onClose={handleCloseModal}
+          onSubmit={handleSubmitInviteMember}
+        />
       </div>
-      {/* 대시보드 삭제 */}
-      <div>
-        <button
-          type='submit'
-          className='mobile:max-w-2xs my-6 ml-5 h-12 w-xs cursor-pointer rounded-sm border border-gray-300 px-4 py-2 text-sm font-medium hover:bg-gray-100'
-          onClick={() => {
-            alert('대시보드를 삭제하시겠습니까?');
-          }}
-        >
-          대시보드 삭제하기
-        </button>
-      </div>
-      <InviteMemberModal
-        isOpen={isModalOpen}
-        onClose={handleCloseModal}
-        onSubmit={handleSubmitInviteMember}
-      />
     </div>
   );
 }
